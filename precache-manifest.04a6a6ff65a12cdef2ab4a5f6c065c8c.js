66eeaad275dfe72ccfa1e7777586a4b283b8a232
Dann Stockton <dannstockton@i3logix.com>
2019-01-20 15:28:48 -0700

    Updates

---
+++ precache-manifest.04a6a6ff65a12cdef2ab4a5f6c065c8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb5e9d0970dd41df084",
+    "url": "/wj/static/js/runtime~main.adb5e9d0.js"
+  },
+  {
+    "revision": "ff9c619f4f80971ea1ac",
+    "url": "/wj/static/js/main.ff9c619f.chunk.js"
+  },
+  {
+    "revision": "45bb8a8af502d1fa1f17",
+    "url": "/wj/static/js/1.45bb8a8a.chunk.js"
+  },
+  {
+    "revision": "ff9c619f4f80971ea1ac",
+    "url": "/wj/static/css/main.9936dcc5.chunk.css"
+  },
+  {
+    "revision": "170b751331994b11ae823f479929b936",
+    "url": "/wj/index.html"
+  }
+];
