5915cf989f8a9484aaebfd46e752359059952da2
Dann Stockton <dannstockton@i3logix.com>
2019-01-20 15:12:47 -0700

    Fetch random images, blur bg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { Parallax } from 'react-parallax';
-import { quotes, defaultAttribution } from './data/quotes';
+import { quotes, defaultAttribution, imageFetcher, sleep } from './data/quotes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    };
+  }
+  fetchImage = () => {
+    imageFetcher().then(async url => {
+      if (!this.state.images.includes(url)) {
+        this.setState({ images: [...this.state.images, url] });
+      }
+      if (this.state.images.length !== quotes.length) {
+        await sleep(500);
+        this.fetchImage();
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.fetchImage();
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,14 +33,20 @@ class App extends Component {
           <div className="header">Probably Jesus</div>
           <div className="main">
             <div>
-              {quotes.map((quote,index) => (
+              {quotes.map((quote, index) => (
                 <div key={index}>
-                  <Parallax bgImage={quote.image} strength={500}>
+                  <Parallax
+                    className={'parallax'}
+                    bgImage={this.state.images[index] || ''}
+                    strength={500}
+                  >
                     <div className={'quote-container'}>
                       <p className={'callout'}>
                         "{quote.text}"
                         <br />
-                        <span className={'attribution'}>– {quote.attribution || defaultAttribution}</span>
+                        <span className={'attribution'}>
+                          – {quote.attribution || defaultAttribution}
+                        </span>
                       </p>
                     </div>
                   </Parallax>
--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,35 +1,38 @@
 const defaultAttribution = 'Jesus, probably';
-const quotes = [
-    {
-        text:
-            "Remember two things. Number one, I said, we're going to be saying Christmas again. And, number two, I said I was going to give you a Christmas present.",
-        image: '/img/thomas-millot-1297305-unsplash.jpg',
-    },
-    {
-        text:
-            'You go to the hospital. You have a broken arm. You come out, you are a drug addict with this crap.',
-        image: '/img/adrien-olichon-1297123-unsplash.jpg',
-    },
-    {
-        text: "We're going to win at space.",
-        image: '/img/ben-carless-1297417-unsplash.jpg',
-    },
-    {
-        text: 'That just shows when you get good ratings, you can say anything.',
-        image: '/img/dynamic-wang-1299294-unsplash.jpg',
-    },
-    {
-        text:
-            "To be honest – I'll be honest, I thought it was a – I thought it was a very close to complete victory.",
-        image: '/img/mirna-rivalta-1295888-unsplash.jpg',
-        attribution: 'Jesus to the Devil, probably'
-    },
-    {
-        text:
-            "He may have had a bad past. Who knows, you know? He may have had some, I think, accusations made. Didn't he have a couple things in his past?",
-        image: '/img/adrien-olichon-1286889-unsplash.jpg',
-        attribution: 'Jesus about Judas, probably'
-    }
+let quotes = [
+  {
+    text:
+      "Remember two things. Number one, I said, we're going to be saying Christmas again. And, number two, I said I was going to give you a Christmas present."
+  },
+  {
+    text:
+      "Remember two things. Number one, I said, we're going to be saying Christmas again. And, number two, I said I was going to give you a Christmas present."
+  },
+  {
+    text:
+      'You go to the hospital. You have a broken arm. You come out, you are a drug addict with this crap.'
+  },
+  {
+    text: "We're going to win at space."
+  },
+  {
+    text: 'That just shows when you get good ratings, you can say anything.'
+  },
+  {
+    text:
+      "To be honest – I'll be honest, I thought it was a – I thought it was a very close to complete victory.",
+    attribution: 'Jesus to the Devil, probably'
+  },
+  {
+    text:
+      "He may have had a bad past. Who knows, you know? He may have had some, I think, accusations made. Didn't he have a couple things in his past?",
+    attribution: 'Jesus about Judas, probably'
+  }
 ];
 
-export {quotes, defaultAttribution};
+let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const imageFetcher = () => {
+  return fetch('https://source.unsplash.com/random').then(res => res.url);
+};
+
+export { quotes, defaultAttribution, imageFetcher, sleep };
